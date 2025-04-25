d0faa96b6755d0697d1abfcce1c5f22b16c62c6d
qilin102223 <qilin102223@gmail.com>
2025-04-25 16:14:29 +0800

    feat: Astro Starlight setup

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'eduroamTW 第三方資料庫',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/eduroamtw' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
